50b39a9824c512949edf0078d939bb59787e73eb
Tamas Lecza <ocbtomi36@outlook.hu>
2026-06-29 10:42:26 +0200

    error handling by userController is refactored

--- controller/userController.js
+++ controller/userController.js
@@ -10,13 +10,13 @@ exports.getAllUsers = async (req,res,next) => {
             return res.status(200).json({message: 'There is no data in database'})
         }
         } catch (error) {
-            return res.status(500).json({message: error.message})
+            return next(error);
         }
 }
 exports.getOneUser = async (req,res,next) => {
     try {
         return res.status(200).json({message: 'Querry success', data: req.user});
         } catch (error) {
-        return res.status(500).json({message: error.message})
+        return next(error);
         }
 }
